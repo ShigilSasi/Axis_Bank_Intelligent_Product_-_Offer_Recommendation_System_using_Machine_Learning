f8958742eb3f4684315267458cbd162d0e5768a2
ShigilSasi <shigilsasi2000@gmail.com>
2026-02-18 14:20:49 +0530

    Backend

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from app.model_loader import load_models
 
-app = FastAPI(
-    title="Axis Bank Intelligent Product & Offer Recommendation System"
-)
+app = FastAPI(title="Axis Bank Intelligent Product & Offer Recommendation System")
+
+@app.on_event("startup")
+def startup_event():
+    load_models()
 
 @app.get("/")
 def home():
-    return {
-        "message": "Welcome to Axis Bank Intelligent Product & Offer Recommendation System"
-    }
+    return {"message": "API is running successfully"}
---
+++ Backend/app/model_loader.py
@@ -0,0 +1,21 @@
+import joblib
+import os
+
+models = {}
+
+def load_models():
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    artifacts_path = os.path.join(base_dir, "artifacts")
+
+    try:
+        models["card_model"] = joblib.load(os.path.join(artifacts_path, "card_model_rf.joblib"))
+        models["churn_model"] = joblib.load(os.path.join(artifacts_path, "churn_model_rf.joblib"))
+        models["loan_model"] = joblib.load(os.path.join(artifacts_path, "loan_model_rf.joblib"))
+        models["offer_model"] = joblib.load(os.path.join(artifacts_path, "offer_model_rf.joblib"))
+        models["kmeans_model"] = joblib.load(os.path.join(artifacts_path, "unsup_kmeans.joblib"))
+        models["scaler"] = joblib.load(os.path.join(artifacts_path, "unsup_scaler.joblib"))
+
+        print("All models loaded successfully.")
+
+    except Exception as e:
+        print("Error loading models:", str(e))
