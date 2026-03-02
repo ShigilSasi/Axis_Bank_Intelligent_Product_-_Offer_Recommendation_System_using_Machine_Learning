e76da094ccf5cdef5ecfa6b1cfe9c664625ecdb8
ShigilSasi <shigilsasi2000@gmail.com>
2026-03-02 10:35:54 +0530

    Updating routes

--- Backend/app/api/routes/predict.py
+++ Backend/app/api/routes/predict.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
-from app.schemas.predict_schema import PredictRequest
+from fastapi import APIRouter, HTTPException
+from app.schemas.predict_schema import PredictRequest, PredictResponse
 from app.services.predict import predict_user
 
 router = APIRouter(prefix="/predict", tags=["Prediction"])
 
-@router.post("/")
+@router.post("/", response_model=PredictResponse)
 def predict(data: PredictRequest):
-    result = predict_user(data.dict())
-    return result
+    try:
+        result = predict_user(data.account_id)
+        return result
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
