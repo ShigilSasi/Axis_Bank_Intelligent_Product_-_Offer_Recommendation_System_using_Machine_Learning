a363f11c064b0135b70fd30f3479ca3996ce9a90
ShigilSasi <shigilsasi2000@gmail.com>
2026-03-02 10:27:54 +0530

    Features to FastApi

--- Backend/app/schemas/predict_schema.py
+++ Backend/app/schemas/predict_schema.py
@@ -1,4 +1,23 @@
 from pydantic import BaseModel
+from typing import List, Dict
 
+# ================= INPUT =================
 class PredictRequest(BaseModel):
     account_id: str
+
+# ================= OUTPUT =================
+class RecommendationResponse(BaseModel):
+    credit_cards: List[str]
+    loans: List[str]
+    offers: List[str]
+
+class PredictResponse(BaseModel):
+    account_id: str
+    cluster: int
+    cluster_label: str
+    loan_eligible: int
+    card_suitable: int
+    offer_eligible: int
+    churn_risk: int
+    churn_probability: float
+    recommendations: Dict[str, List[str]]
