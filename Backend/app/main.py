368b786c7c978182fc06d58e92fd056d01d02389
ShigilSasi <shigilsasi2000@gmail.com>
2026-03-02 10:38:44 +0530

    Main Api Endpoint

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from app.api.routes.predict import router as predict_router
 from app.models.model_loader import load_models
 
-app = FastAPI()
+load_dotenv()
+
+app = FastAPI(title="Axis Bank ML System")
 
 @app.on_event("startup")
 def startup_event():
     load_models()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 app.include_router(predict_router)
